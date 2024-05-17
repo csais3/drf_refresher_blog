6d6d87ad7e7798c8844000f2169113131f2a6cf9
Csais <csais1986@gmail.com>
2024-05-17 07:41:26 -0700

    first commit, project shell

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)

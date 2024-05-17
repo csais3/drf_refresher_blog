4a3ab2971b9fcee84feb9f03fcd848a2f01f7e1b
csais3 <108033096+csais3@users.noreply.github.com>
2024-05-17 11:23:10 -0700

    added test to endpoints and model creation

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,30 @@ class Category(models.Model):
         return self.name
     
 class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('draft', 'draft'),
+        ('published', 'published')
+    )
+
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    status = models.CharField(max_length=10, choices=options, default='published')
+    objects = models.Manager() # default manager
+    postobjects = PostObjects() # custom manager
+
+
+    class Meta:
+        ordering = ('-published',)
+    
+    def __str__(self) -> str:
+        return self.title
